ec4c03bcf5318dc2d5846f3a0977364983eeaabb
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-26 21:48:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ function createTable() {
 	let rows=prompt("Input number of rows");
 	let col=prompt("Input number of columns");
 
-	console.log(rows,col);
+
+	let tableELe=document.getElementById("myTable");
+	tableELe.innerHTML="";
+	tableELe.innerHTML=`<thead></thead>
+                       <tbody></tbody>`;
+	//console.log(tableELe);
+	let tbody=document.getElementById("myTable").children[1];
+	for(let i=0;i<rows;i++){
+		let newTr=document.createElement("tr");
+		for(let j=0;j<col;j++){
+			let newTd=document.createElement("td");
+			newTd.textContent=`Row-${i} Column-${j}`;
+			newTr.appendChild(newTd);
+		}
+		tbody.appendChild(newTr);
+	}
   
 }
